a65289880541439a7d88bd6096c2829823621039
itsaliceon <itsaliceon@users.noreply.github.com>
2019-03-25 16:02:07 -0400

    3.0

--- 00 - Practice Folder/02 - Wordpress Theme Development/footer.php
+++ 00 - Practice Folder/02 - Wordpress Theme Development/footer.php
@@ -1,9 +1,10 @@
-            <aside class="col-md-4 blog-sidebar">
-               <?php if(is_active_sidebar('sidebar')): ?>
-                    <?php dynamic_sidebar('sidebar'); ?>
-                <?php endif; ?>
-            </aside><!-- /.blog-sidebar -->
-
+            <?php if(!is_front_page()) : ?>
+                <aside class="col-md-4 blog-sidebar">
+                <?php if(is_active_sidebar('sidebar')): ?>
+                        <?php dynamic_sidebar('sidebar'); ?>
+                    <?php endif; ?>
+                </aside><!-- /.blog-sidebar -->
+            <?php endif; ?>
         </div><!-- /.row -->
 
     </main><!-- /.container -->
